e2ff85d64a4b38ebe895181b3ce3fb4b6aaef757
Fabio Rodriguez <alessf13@gmail.com>
2022-04-07 14:58:15 +0100

    ex5: More Variables with format string

---
+++ ex5.py
@@ -0,0 +1,18 @@
+name = 'Fabio Rodriguez'
+age = 30
+height = 165 # cm
+weight = 69.3 # kg
+eyes= 'Brown'
+teeth = 'Whitish'
+hair = 'Brown'
+
+print(f"Let's tals about {name}")
+print(f"He's {height} cms tall.")
+print(f"He's {weight} kg heavy.")
+print(f"Actually that's not to heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+total = age + height + weight
+
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
